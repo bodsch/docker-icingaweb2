f599b82a5407aee7bc5844f334909ffee2050faa
Bodo Schulz <bodo@boone-schulz.de>
2017-04-03 16:31:16 +0200

    function configureIcingaGraphite() is a lite bit wrong

--- rootfs/opt/startup.sh
+++ rootfs/opt/startup.sh
@@ -244,6 +244,10 @@ EOF
 
 configureIcingaGraphite() {
 
+  echo "[i] - currently disabled"
+
+  return
+
   if [ ! -d /etc/icingaweb2/modules/graphite/templates ]
   then
     cp -arv /usr/share/webapps/icingaweb2/modules/graphite/sample-config/icinga2/* /etc/icingaweb2/modules/graphite/
@@ -251,8 +255,10 @@ configureIcingaGraphite() {
 
   if [ -f /etc/icingaweb2/modules/graphite/config.ini ]
   then
+    # TODO
+    # fix it
     sed -i \
-      -e 's|my.graphite.web|graphite:8080|g' \
+      -e "s|my.graphite.web|${GRAPHITE_HOST}:${GRAPHITE_PORT}|g" \
       /etc/icingaweb2/modules/graphite/config.ini
   fi
 }
